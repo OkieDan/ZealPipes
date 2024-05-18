07ff0982650668d77bf0352d5760024630f76622
OkieDan <137017569+OkieDan@users.noreply.github.com>
2024-05-17 20:54:13 -0500

    Get rid of an async void, and use thread safe collections for process and character list. Other cleanup. (#13)

--- ZealPipes.Services/ZealMessageService.cs
+++ ZealPipes.Services/ZealMessageService.cs
@@ -1,32 +1,33 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using ZealPipes.Common;
 using ZealPipes.Common.Models;
 using ZealPipes.Services.Helpers;
 using static ZealPipes.Services.Helpers.ZealPipeReader;
+
 namespace ZealPipes.Services
 {
     public partial class ZealMessageService
     {
         private readonly ProcessMonitor _processMonitor;
-        private readonly ZealSettings _zealSettings;
         private readonly ZealPipeReader _zealPipeReader;
-        private List<int> _processList = new List<int>();
+        private readonly ConcurrentDictionary<int, byte> _processList = new ConcurrentDictionary<int, byte>();
+        private readonly ConcurrentBag<ZealCharacter> _characterList = new ConcurrentBag<ZealCharacter>();
+
         public event EventHandler<LogMessageReceivedEventArgs> OnLogMessageReceived;
         public event EventHandler<LabelMessageReceivedEventArgs> OnLabelMessageReceived;
         public event EventHandler<GaugeMessageReceivedEventArgs> OnGaugeMessageReceived;
         public event EventHandler<PlayerMessageReceivedEventArgs> OnPlayerMessageReceived;
         public event EventHandler<ZealCharacter.ZealCharacterUpdatedEventArgs> OnCharacterUpdated;
         public event EventHandler<PipeCmdMessageReceivedEventArgs> OnPipeCmdMessageReceived;
-        public List<ZealCharacter> CharacterList = new List<ZealCharacter>();
 
-        public ZealMessageService(IConfiguration configuration, ProcessMonitor processMonitor, ZealSettings zealSettings, ZealPipeReader zealPipeReader)
+        public ZealMessageService(IConfiguration configuration, ProcessMonitor processMonitor, ZealPipeReader zealPipeReader)
         {
             _processMonitor = processMonitor;
-            _zealSettings = zealSettings;
             _zealPipeReader = zealPipeReader;
             _processMonitor.OnNewProcessFound += ProcessMonitor_OnNewProcessFound;
             _zealPipeReader.OnPipeMessageReceived += ZealPipeReader_OnPipeMessageReceived;
@@ -35,72 +36,62 @@ namespace ZealPipes.Services
 
         private void ProcessMonitor_OnNewProcessFound(object sender, ProcessMonitor.NewProcessFoundEvent e)
         {
-            _processList.Add(e.ProcessId);
-            new Thread(() => { _zealPipeReader.StartReading(e.ProcessId); }).Start();
+            if (_processList.TryAdd(e.ProcessId, 0))
+            {
+                Task.Run(() => _zealPipeReader.StartReading(e.ProcessId));
+            }
+        }
+
+        private ZealCharacter GetOrCreateCharacter(int processId, string characterName)
+        {
+            var character = _characterList.FirstOrDefault(x => x.ProcessId == processId && x.Name == characterName);
+            if (character == null)
+            {
+                character = new ZealCharacter(characterName, processId);
+                character.OnCharacterUpdated += Character_OnCharacterUpdated;
+                _characterList.Add(character);
+            }
+            return character;
         }
 
         private void ZealPipeReader_OnPipeMessageReceived(object sender, PipeMessageReceivedEventArgs e)
         {
-            //Console.WriteLine($"Message from Zeal Pipe {e.ProcessId}: {e.Message.Character}: {e.Message.Type}: {e.Message.DataLen}: {e.Message.Data}");
-            PipeMessageType pipeMessageType = (PipeMessageType)e.Message.Type;
-            ZealCharacter character;
+            var message = e.Message;
+            var characterName = message.Character;
+            var processId = e.ProcessId;
+            var data = message.Data;
+
+            var character = GetOrCreateCharacter(processId, characterName);
+            var pipeMessageType = (PipeMessageType)message.Type;
+
             switch (pipeMessageType)
             {
-                case PipeMessageType.LogText: // log
-                    OnLogMessageReceived?.Invoke(this, new LogMessageReceivedEventArgs(
-                            e.ProcessId,
-                            new LogMessage(e.Message.Character,e.Message.Data)                            
-                        ));
+                case PipeMessageType.LogText:
+                    OnLogMessageReceived?.Invoke(this, new LogMessageReceivedEventArgs(processId, new LogMessage(characterName, data)));
                     break;
-                case PipeMessageType.Label: // label
-                    var labelMessage = new LabelMessage(e.Message.Character, e.Message.Data);
-                    character = CharacterList.Where(x => x.ProcessId == e.ProcessId && x.Name == labelMessage.Character).FirstOrDefault();
-                    if (character == null)
-                    {
-                        character = new ZealCharacter(labelMessage.Character, e.ProcessId);
-                        character.OnCharacterUpdated += Character_OnCharacterUpdated;
-                        CharacterList.Add(character);
-                    }
-                    character.UpdateCharacterData(labelMessage.Data);
-                    OnLabelMessageReceived?.Invoke(this, new LabelMessageReceivedEventArgs(
-                            e.ProcessId,
-                            labelMessage
-                        ));
-                    break;
-                case PipeMessageType.Gauge: // gauge
-                    var gaugeMessage = new GaugeMessage(e.Message.Character, e.Message.Data);
-                    character = CharacterList.Where(x => x.ProcessId == e.ProcessId && x.Name == gaugeMessage.Character).FirstOrDefault();
-                    if (character == null)
-                    {
-                        character = new ZealCharacter(gaugeMessage.Character, e.ProcessId);
-                        character.OnCharacterUpdated += Character_OnCharacterUpdated;
-                        CharacterList.Add(character);
-                    }
-                    character.UpdateCharacterData(gaugeMessage.Data);
-                    OnGaugeMessageReceived?.Invoke(this, new GaugeMessageReceivedEventArgs(
-                            e.ProcessId,
-                            new GaugeMessage(e.Message.Character, e.Message.Data)                            
-                        ));
-                    break;
-                case PipeMessageType.Player:
-                    var playerMessage = new PlayerMessage(e.Message.Character, e.Message.Data);
-                    character = CharacterList.Where(x => x.ProcessId == e.ProcessId && x.Name == playerMessage.Character).FirstOrDefault();
-                    character.UpdateCharacterData(playerMessage.Data);
 
-                    OnPlayerMessageReceived?.Invoke(this, new PlayerMessageReceivedEventArgs(
-                            e.ProcessId,
-                            new PlayerMessage(e.Message.Character, e.Message.Data)
-                        ));
-                    break;
                 case PipeMessageType.PipeCmd:
-                    OnPipeCmdMessageReceived?.Invoke(this, new PipeCmdMessageReceivedEventArgs(
-                            e.ProcessId,
-                            new PipeCmdMessage(e.Message.Character, e.Message.Data)
-                        ));
+                    OnPipeCmdMessageReceived?.Invoke(this, new PipeCmdMessageReceivedEventArgs(processId, new PipeCmdMessage(characterName, data)));
                     break;
 
+                case PipeMessageType.Label:
+                    var labelMessage = new LabelMessage(characterName, data);
+                    character.UpdateCharacterData(labelMessage.Data);
+                    OnLabelMessageReceived?.Invoke(this, new LabelMessageReceivedEventArgs(processId, labelMessage));
+                    break;
+
+                case PipeMessageType.Gauge:
+                    var gaugeMessage = new GaugeMessage(characterName, data);
+                    character.UpdateCharacterData(gaugeMessage.Data);
+                    OnGaugeMessageReceived?.Invoke(this, new GaugeMessageReceivedEventArgs(processId, gaugeMessage));
+                    break;
+
+                case PipeMessageType.Player:
+                    var playerMessage = new PlayerMessage(characterName, data);
+                    character.UpdateCharacterData(playerMessage.Data);
+                    OnPlayerMessageReceived?.Invoke(this, new PlayerMessageReceivedEventArgs(processId, playerMessage));
+                    break;
             }
-            // Further process the message here...
         }
 
         private void Character_OnCharacterUpdated(object sender, ZealCharacter.ZealCharacterUpdatedEventArgs e)
@@ -112,13 +103,14 @@ namespace ZealPipes.Services
         {
             _processMonitor.StartMonitoring();
         }
+
         public void StopProcessing()
         {
             _processMonitor.StopMonitoring();
-            _processList.ForEach(x =>
+            foreach (var processId in _processList.Keys)
             {
-                _zealPipeReader.StopReading(x);
-            });
+                _zealPipeReader.StopReading(processId);
+            }
         }
     }
 }
